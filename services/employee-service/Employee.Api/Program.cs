f9d51b5c7f1a87fdc832544f0b7ed870ee6db42f
ushasree210 <79778957+ushasree210@users.noreply.github.com>
2026-01-26 23:45:23 -0600

    Infrasetup (#8)
    * add missing rollback command
    
    * infra setup

--- services/employee-service/Employee.Api/Program.cs
+++ services/employee-service/Employee.Api/Program.cs
@@ -1,5 +1,7 @@
 
+using Employee.Api.Infrastructure.Data;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 
 namespace Employee.Api
 {
@@ -8,6 +10,15 @@ namespace Employee.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args); //Think of builder as: “setup mode”
+
+
+            builder.Configuration.AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true);
+
+            var conn = builder.Configuration.GetConnectionString("EmployeeDb");
+            if (string.IsNullOrWhiteSpace(conn))
+                throw new InvalidOperationException("Missing connection string: ConnectionStrings:EmployeeDb");
+
+            builder.Services.AddDbContext<EmployeeDbContext>(opt => opt.UseNpgsql(conn));
 
             // Add services to the container.
             // These lines prepare what your app can do.
@@ -19,14 +30,16 @@ namespace Employee.Api
 
            
 
+
             var app = builder.Build(); // Now the app is created.
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
+            Console.WriteLine($"ENV = {app.Environment.EnvironmentName}");
+            Console.WriteLine($"DB  = {builder.Configuration.GetConnectionString("EmployeeDb")}");
+
+
+            app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            
             // Only in Development environment: creates / swagger / v1 / swagger.json shows Swagger UI at / swagger So Swagger is not enabled in production by default(security best practice).
             
             app.UseHttpsRedirection(); // If someone hits HTTP, redirect to HTTPS and  Helps with security
